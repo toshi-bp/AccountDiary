90e18179e68d9b489ec75c9f7c85113a47c80e8b
10shiba <7419045@ed.tus.ac.jp>
2021-10-22 18:08:28 +0900

    1022_makeDB

--- backend/N_models.py
+++ backend/N_models.py
@@ -46,7 +46,7 @@ class images(db.Model):
     image_url = db.Column(db.String(255), nullable=False)
     act_time = db.Column(db.DateTime, nullable=False)
     update_time = db.Column(db.DateTime, nullable=False)
-    diary = db.Column(db.String, nullable=False)
+    diary = db.Column(db.String(255), nullable=False)
 
     def __init__(self, id, member_id, image_url, act_time, update_time, diary):
         self.id = id
@@ -57,3 +57,50 @@ class images(db.Model):
         self.diary = diary
     
     # 抽象クラスを作成した方がわかりやすい...？
+
+#メンバーシップに関するDB
+class membership(db.Model):
+    #テーブル名
+    __tablename__ = 'membership'
+
+    # カラム情報
+    id = db.Column(db.Integer, primary_key=True)
+    mail = db.Column(db.String(255))
+    pwd = db.Column(db.String(255), nullable=False)
+    act_birthday = db.Column(db.Date, nullable=False)
+    gender = db.Column(db.String(255), nullable=False)
+    phonenumber = db.Column(db.String(255), nullable=False)
+
+    def __init__(self, id, mail, pwd, birthday, gender, phonenumber):
+        self.id = id
+        self.mail = mail
+        self.pwd = pwd
+        self.act_birthday = birthday
+        self.gender = gender
+        self.phonenumber = phonenumber
+
+class history(db.Model):
+    #テーブル名
+    __tablename__ = 'history'
+
+    # カラム情報
+    id = db.Column(db.Integer, primary_key=True)
+    memberId = db.Column(db.Integer)
+    action = db.Column(db.String(255), nullable=False)
+    result = db.Column(db.Integer, nullable=False)
+    act_time = db.Column(db.Date, nullable=False)
+    update_time = db.Column(db.Date, nullable=False)
+    category = db.Column(db.String(255), nullable=False)
+
+    def __init__(self, id, memberId, action, result, act_time, update_time,category):
+        self.id = id
+        self.memberId = memberId
+        self.action= action
+        self.result = result
+        self.act_time = act_time
+        self.update_time = update_time
+        self.category = category
+
+# default = datetime.now を使用するとわかりやすいかも
+
+

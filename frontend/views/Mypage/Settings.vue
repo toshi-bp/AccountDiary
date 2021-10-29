751d07c31183128cf5941736761a7893701ebf34
tatataishin <7419052@ed.tus.ac.jp>
2021-10-29 17:58:07 +0900

    Co-authored-by: kurakuramaru <kurakuramaru@users.noreply.github.com> Co-authored-by: shiotsu12 <shiotsu12@users.noreply.github.com> Co-authored-by: 10shiba <10shiba@users.noreply.github.com>

---
+++ frontend/views/Mypage/Settings.vue
@@ -0,0 +1,40 @@
+<template>
+
+<div>
+ <center>
+ <el-select v-model="value" placeholder="Select">
+    <el-option
+      v-for="item in options"
+      :key="item.value"
+      :label="item.label"
+      :value="item.value">
+    </el-option>
+ </el-select>
+ </center>
+ 
+</div>
+
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        options: [{
+          value: 'Option1',
+          label: 'Option1'
+        }, {
+          value: 'Option2',
+          label: 'Option2'
+        }, {
+          value: 'Option3',
+          label: 'Option3'
+        }, {
+          value: 'Option4',
+          label: 'Option4'
+        }, ],
+        value: ''
+      }
+    }
+  }
+</script>
